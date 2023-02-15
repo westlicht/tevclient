dd0bcdda6de7a12d61fbe836cad546f89f86b5f3
Simon Kallweit <simon.kallweit@gmail.com>
2023-02-15 23:53:19 +0100

    update license

--- include/tevclient.h
+++ include/tevclient.h
@@ -1,4 +1,4 @@
-// This file was developed by Thomas Müller <thomas94@gmx.net> and Simon Kallweit <simon.kallweit@gmail.com>.
+// This file was developed by Simon Kallweit and Thomas Müller <thomas94@gmx.net>.
 // It is published under the BSD 3-Clause License within the LICENSE file.
 
 #pragma once
